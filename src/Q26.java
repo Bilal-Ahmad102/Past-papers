472784cd3d72381264b122a9ad3d0784ccb46fb6
Bilal Ahmad <ba2736180@gmail.com>
2024-01-14 23:09:25 +0500

    add binary

--- src/Q26.java
+++ src/Q26.java
@@ -1,8 +1,40 @@
-class Q26 {
+
+// Decimal to Binary
+
+import java.util.Scanner;
+
+public class Q26 {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int c = a%b;
-        System.out.println(c);        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        int decimalNumber = scanner.nextInt();
+
+        int[] binaryArray = convertToBinaryArray(decimalNumber);
+
+        System.out.print("Binary representation: ");
+        for (int i = binaryArray.length - 1; i >= 0; i--) {
+            System.out.print(binaryArray[i]);
+        }
+        System.out.println();
+    }
+
+    public static int[] convertToBinaryArray(int decimalNumber) {
+        // Determine the number of bits needed
+        int[] binaryArray = new int[32];
+        int i = 0;
+
+        while (decimalNumber >= 0) {
+            if(decimalNumber == 1){
+                binaryArray[i] = 1;
+                break; 
+            }
+            binaryArray[i] = decimalNumber%2;
+            decimalNumber /= 2;
+            i++;
+        }
+
+
+        return binaryArray;
     }
 }
